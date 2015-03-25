c16c66d8987050f49e85c19542af62c761c507a7
Mariano Montone <marianomontone@gmail.com>
2015-03-25 00:40:25 -0300

    define-format-operation

--- format-ext.asd
+++ format-ext.asd
@@ -1,10 +1,9 @@
-;;;; format-ext.asd
-
 (asdf:defsystem #:format-ext
   :description "Describe format-ext here"
   :author "Mariano Montone"
   :license "MIT"
   :serial t
   :components ((:file "package")
-               (:file "format-ext")))
+               (:file "format-ext"))
+  :depends-on (:alexandria :esrap))
 
--- format-ext.lisp
+++ format-ext.lisp
@@ -39,45 +39,55 @@
   (loop for clause in clauses
        do (format-clause destination clause)))
 
+(defvar *format-operations* (make-hash-table :test #'equalp))
+
+(defstruct format-operation
+  name
+  keywords
+  format
+  compile
+  documentation)
+
+(defmacro define-format-operation (name &body options)
+  (flet ((extract-option (name)
+	   (cdr (find name options :key #'car))))
+    (alexandria:with-unique-names (format-operation keyword)
+      `(let ((,format-operation (make-format-operation
+				 :name ',name
+				 :keywords ',(first (extract-option :keywords))
+				 :format ,(let ((format (extract-option :format)))
+					   `(lambda ,(first format)
+					      ,@(rest format)))
+				 :compile ,(let ((compile (extract-option :compile)))
+						`(lambda ,(first compile)
+						   ,@(rest compile)))
+				 :documentation ,(first (extract-option :documentation)))))
+	 ,@(loop for keyword in (first (extract-option :keywords))
+	      collect `(setf (gethash ,keyword *format-operations*)
+			     ,format-operation))))))
+
+(defun find-format-operation (keyword)
+  (gethash keyword *format-operations*))
+
 (defmethod format-clause (destination (clause string))
   (write-string clause destination))
 
 (defmethod format-clause (destination (clause character))
   (write-char clause destination))
 
+(defmethod format-clause (destination (clause (eql :newline)))
+  (terpri destination))
+
 (defmethod format-clause (destination (clause cons))
-  (format-operation destination
-		    (first clause)
-		    clause))
+  (let ((format-operation (find-format-operation (first clause))))
+    (if format-operation
+	(funcall (format-operation-format format-operation)
+		 destination
+		 clause)
+	(error "Invalid format operation: ~A" (first clause)))))
 
 (defmethod format-clause (destination clause)
   (princ clause destination))
-
-(defgeneric format-operation (destination operation clause)
-  (:method (destination operation clause)
-    (error "Invalid format operation: ~S" operation)))
-  
-(defmethod format-operation (destination (operation (eql :s)) clause)
-  (prin1 (cadr clause) destination))
-
-(defmethod format-operation (destination (operation (eql :a)) clause)
-  (princ (cadr clause) destination))
-
-(defmethod format-operation (destination (operation (eql :when)) clause)
-  (destructuring-bind (_ condition &rest body) clause
-    (when condition
-      (loop for clause in body
-	   do (format-clause destination clause)))))
-
-(defmethod format-operation (destination (operation (eql :join)) clause)
-  (destructuring-bind (_ separator args) clause
-    (when args
-      (loop 
-	 for arg in (butlast args)
-	 do 
-	   (format-clause destination arg)
-	   (format-clause destination separator)
-	 finally (format-clause destination (car (last args)))))))
 
 (defmethod compile-clause (destination (clause string))
   `(write-string ,clause ,destination))
@@ -85,40 +95,81 @@
 (defmethod compile-clause (destination (clause character))
   `(write-char ,clause ,destination))
 
+(defmethod compile-clause (destination (clause (eql :newline)))
+  `(terpri ,destination))
+
 (defmethod compile-clause (destination (clause cons))
   (if (keywordp (first clause))
-      (compile-operation destination
-			 (first clause)
-			 clause)
-      clause))
+      (let ((format-operation (find-format-operation (first clause))))
+	(if format-operation
+	    (funcall (format-operation-compile format-operation)
+		     destination
+		     clause)
+	    (error "Invalid format operation: ~A" (first clause))))
+      `(princ ,clause ,destination)))
 
 (defmethod compile-clause (destination clause)
   `(princ ,clause ,destination))
 
-(defgeneric compile-operation (destination operation clause)
-  (:method (destination operation clause)
-    (error "Invalid format operation: ~S" operation)))
-  
-(defmethod compile-operation (destination (operation (eql :s)) clause)
-  `(prin1 ,(cadr clause) ,destination))
+(define-format-operation standard
+    (:keywords (:s :std :standard))
+    (:format (destination clause)
+	     (prin1 (cadr clause) destination))
+    (:compile (destination clause)
+	      `(prin1 ,(cadr clause) ,destination))
+    (:documentation "Standard print"))
 
-(defmethod compile-operation (destination (operation (eql :a)) clause)
-  `(princ ,(cadr clause) ,destination))
+(define-format-operation aesthetic
+  (:keywords (:a :aesthetic))
+  (:format (destination clause)
+	   (princ (cadr clause) destination))
+  (:compile (destination clause)
+	    `(princ ,(cadr clause) ,destination))
+  (:documentation "Aesthetic print"))
 
-(defmethod compile-operation (destination (operation (eql :when)) clause)
-  (destructuring-bind (_ condition &rest body) clause
-    `(when ,condition
-       ,@(loop for clause in body
-	    collect (compile-clause destination clause)))))
+(define-format-operation when
+  (:keywords (:w :when))
+  (:format (destination clause)
+	   (destructuring-bind (_ condition &rest body) clause
+	     (when condition
+	       (loop for clause in body
+		  do (format-clause destination clause)))))
+  (:compile (destination clause)
+	    (destructuring-bind (_ condition &rest body) clause
+	      `(when ,condition
+		 ,@(loop for clause in body
+		      collect (compile-clause destination clause)))))
+  (:documentation "Format iff condition is true"))
 
-(defmethod compile-operation (destination (operation (eql :join)) clause)
-  (destructuring-bind (_ separator args) clause
-    (alexandria:with-unique-names (arg)
-      (alexandria:once-only (args)
-	`(when ,args
-	   (loop 
-	      for ,arg in (butlast ,args)
-	      do 
-		(format-clause ,destination ,arg)
-		(format-clause ,destination ,separator)
-	      finally (format-clause ,destination (car (last ,args)))))))))
+(define-format-operation join
+  (:keywords (:+ :j :join :concat :slice))
+  (:format (destination clause)
+	   (destructuring-bind (_ separator args) clause
+	     (when args
+	       (loop 
+		  for arg in (butlast args)
+		  do 
+		    (format-clause destination arg)
+		    (format-clause destination separator)
+		  finally (format-clause destination (car (last args)))))))
+  (:compile (destination clause)
+	    (destructuring-bind (_ separator args) clause
+	      (alexandria:with-unique-names (arg)
+		(alexandria:once-only (args)
+		  `(when ,args
+		     (loop 
+			for ,arg in (butlast ,args)
+			do 
+			  (format-clause ,destination ,arg)
+			  (format-clause ,destination ,separator)
+			finally (format-clause ,destination (car (last ,args)))))))))
+  (:documentation "Join with separator"))
+
+(defun describe-format-operation (format-operation &optional (stream *standard-output*))
+  (with-format (stream) 
+    (format-operation-name format-operation) " FORMAT OPERATION"
+    :newline
+    "Keywords: " (:join ", " (format-operation-keywords format-operation))
+    :newline
+    :newline
+    (format-operation-documentation format-operation)))
