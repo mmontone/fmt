67d0bbfb8476b767ea3afb5b3476446b4b9e3bd7
Mariano Montone <marianomontone@gmail.com>
2015-04-01 00:00:15 -0300

    Replace string filter

--- fmt.lisp
+++ fmt.lisp
@@ -207,6 +207,29 @@
                  ,arg))
   (:documentation "Truncate filter"))
 
+(defun replace-all (string part replacement &key (test #'char=))
+  "Returns a new string in which all the occurences of the part 
+is replaced with replacement."
+  (with-output-to-string (out)
+    (loop with part-length = (length part)
+       for old-pos = 0 then (+ pos part-length)
+       for pos = (search part string
+			 :start2 old-pos
+			 :test test)
+       do (write-string string out
+			:start old-pos
+			:end (or pos (length string)))
+       when pos do (write-string replacement out)
+       while pos)))
+
+(define-format-filter replace
+  (:keywords (:replace))
+  (:apply (arg part replacement)
+          (replace-all arg part replacement))
+  (:compile (arg part replacement)
+	    `(replace-all ,arg ,part ,replacement))
+  (:documentation "Replace filter"))
+
 (defgeneric format-clause (destination clause))
 
 (defmethod format-clause :around (destination clause)
--- t/tests.lisp
+++ t/tests.lisp
@@ -9,38 +9,38 @@
 
 (test basic-test
   (is (equalp (fmt nil "Hello" #\space "world")
-	      "Hello world"))
+              "Hello world"))
   (is (equalp (fmt nil "Hello" #\newline
-		   "world")
-	      "Hello
+                   "world")
+              "Hello
 world"))
   (is (equalp (fmt* nil "Hello" #\space "world")
-	      "Hello world"))
+              "Hello world"))
   (is (equalp (fmt* nil "Hello" #\newline
-		    "world")
-	      "Hello
+                    "world")
+              "Hello
 world")))
 
 
 (test aesthetic-test
   (is (equalp (fmt nil (:a "foo"))
-	      "foo"))
+              "foo"))
   (is (equalp (fmt nil (:a 22))
-	      "22"))
+              "22"))
   (is (equalp (fmt nil (:aesthetic "foo"))
-	      "foo"))
+              "foo"))
   (is (equalp (fmt nil (:a (list :a :b :c)))
-	      "(A B C)")))
+              "(A B C)")))
 
 (test standard-test
   (is (equalp (fmt nil (:s "foo"))
-	      "\"foo\""))
+              "\"foo\""))
   (is (equalp (fmt nil (:s (list :a :b :c)))
-	      "(:A :B :C)"))
+              "(:A :B :C)"))
   (is (equalp (fmt* nil `(:s ,"foo"))
-	      "\"foo\""))
+              "\"foo\""))
   (is (equalp (fmt* nil `(:s ,(list :a :b :c)))
-	      "(:A :B :C)")))
+              "(:A :B :C)")))
 
 (test when-test
   (let ((cond t))
@@ -50,192 +50,200 @@ world")))
 
 (test join-test
   (is (equalp (fmt nil (:join ", " (list "foo" "bar" "baz")))
-	      "foo, bar, baz"))
+              "foo, bar, baz"))
   (is (equalp (fmt nil (:join #\, (list "foo" "bar")))
-	      "foo,bar"))
+              "foo,bar"))
   (is (equalp (fmt nil (:join "," (list "foo")))
-	      "foo"))
+              "foo"))
   (is (equalp (fmt nil (:join "," (list)))
-	      ""))
+              ""))
   (is (equalp (fmt nil (:join #\space '(a b c)))
-	      "A B C"))
+              "A B C"))
   (is (equalp (fmt nil (:join (", " " and ")
-			      (list "foo" "bar" "baz")))
-	      "foo, bar and baz"))
+                              (list "foo" "bar" "baz")))
+              "foo, bar and baz"))
   (is (equalp (fmt* nil `(:join ", " ,(list "foo" "bar" "baz")))
-	      "foo, bar, baz"))
+              "foo, bar, baz"))
   (is (equalp (fmt* nil `(:join #\, ,(list "foo" "bar")))
-	      "foo,bar"))
+              "foo,bar"))
   (is (equalp (fmt* nil `(:join "," ,(list "foo")))
-	      "foo"))
+              "foo"))
   (is (equalp (fmt* nil `(:join "," ,(list)))
-	      ""))
+              ""))
   (is (equalp (fmt* nil `(:join #\space ,'(a b c)))
-	      "A B C"))
+              "A B C"))
   (is (equalp (fmt* nil `(:join (", " " and ")
-			      ,(list "foo" "bar" "baz")))
-	      "foo, bar and baz")))
+                                ,(list "foo" "bar" "baz")))
+              "foo, bar and baz")))
 
 (test do-test
   (is (equalp (fmt nil (:do (item (list 1 2 3))
-			    (:s item)))
-	      "123")))
+                            (:s item)))
+              "123")))
 
 (test filter-test
   (is (equalp (fmt nil (:a "lala" :upcase))
-	      "LALA"))
+              "LALA"))
   (is (equalp (fmt nil (:s "lala" :up))
-	      "\"LALA\""))
+              "\"LALA\""))
   (is (equalp (fmt nil (:s "lala" #'string-upcase))
-	      "\"LALA\""))
+              "\"LALA\""))
   (is (equalp (fmt* nil `(:a "lala" :upcase))
-	      "LALA"))
+              "LALA"))
   (is (equalp (fmt* nil `(:s "lala" :up))
-	      "\"LALA\""))
+              "\"LALA\""))
   (is (equalp (fmt* nil `(:a "  lala  " (:trim #\ )))
-	      "lala"))
+              "lala"))
   (is (equalp (fmt* nil `(:a "lala" ,#'string-upcase))
-	      "LALA"))
+              "LALA"))
   (is (equalp (fmt* nil `(:a "lala" #'string-upcase))
-	      "LALA")))
+              "LALA")))
 
 (test default-filter-test
   (is (equalp (fmt nil (:a "foo" (:default "bar")))
-	      "foo"))
+              "foo"))
   (is (equalp (fmt nil (:a nil (:default "bar")))
-	      "bar")))
+              "bar")))
 
 (test truncate-filter-test
   (is (equalp (fmt nil (:a "lalalala" (:truncate 10)))
-	      "lalalala"))
+              "lalalala"))
   (is (equalp (fmt nil (:a "lalalala" (:truncate 4)))
-	      "lala..."))
+              "lala..."))
   (is (equalp (fmt nil (:a "lalalala" (:truncate 6 ", etc")))
-	      "lalala, etc")))
+              "lalala, etc")))
+
+(test replace-filter-test
+  (is (equalp (fmt nil (:a "foobar" (:replace "foo" "bar")))
+              "barbar"))
+  (is (equalp (fmt nil (:a "foobar" (:replace "bar" "foo")))
+              "foofoo"))
+  (is (equalp (fmt nil (:a "foobar" (:replace "baz" "foo")))
+              "foobar")))
 
 (test escape-test
   (is (equalp (fmt nil
-		   "hello"
-		   (:esc #\space "beautiful")
-		   #\space "world")
-	      "hello world"))
+                   "hello"
+                   (:esc #\space "beautiful")
+                   #\space "world")
+              "hello world"))
   (is (equalp (fmt* nil
-		    "hello"
-		    `(:esc #\space "beautiful")
-		    #\space
-		    "world")
-	      "hello world")))		   
+                    "hello"
+                    `(:esc #\space "beautiful")
+                    #\space
+                    "world")
+              "hello world")))
 
 (test control-flow-test
-  (is (equalp (fmt nil 
-		   (:a "start")
-		   #\newline
-		   (:esc 
-		    (loop for x in (list 1 2 3)
-			do (:fmt (:s x))))
-		   #\newline
-		   (:a "end"))
-	      "start
+  (is (equalp (fmt nil
+                   (:a "start")
+                   #\newline
+                   (:esc
+                    (loop for x in (list 1 2 3)
+                       do (:fmt (:s x))))
+                   #\newline
+                   (:a "end"))
+              "start
 123
 end")))
 
-(test special-argument-test 
+(test special-argument-test
   (is (equalp (fmt nil (:join ", " (list "a" "b" "c") (:s _)))
-	      "\"a\", \"b\", \"c\""))
+              "\"a\", \"b\", \"c\""))
   (is (equalp (fmt nil (:join ", " (list "a" "b" "c") (:a _ :up)))
-	      "A, B, C")))
+              "A, B, C")))
 
 (test trim-filter-test
   (is (equalp (fmt nil (:a "  hello  " :trim))
-	      "hello"))
+              "hello"))
   (is (equalp (fmt nil (:a "  hello  " (:trim #\ )))
-	      "hello"))
+              "hello"))
   (is (equalp (fmt nil (:a "//hello" (:trim #\/)))
-	      "hello")))
+              "hello")))
 
 (test radix-operation-test
   (is (equalp (fmt nil (:r 4))
-	      "four"))
+              "four"))
   (is (equalp (fmt nil (:r 4 :cardinal))
-	      "four"))
+              "four"))
   (is (equalp (fmt nil (:r 4 :ordinal))
-	      "fourth"))
+              "fourth"))
   (is (equalp (fmt nil (:r 4 2))
-	      "100"))
+              "100"))
   (is (equalp (fmt nil (:r 4 :roman))
-	      "IV"))
+              "IV"))
   (is (equalp (fmt nil (:r 4 :old-roman))
-	      "IIII")))
+              "IIII")))
 
 (test decimal-operation-test
   (is (equalp (fmt nil (:d 23))
-	      "23"))
+              "23"))
   (is (equalp (fmt* nil `(:d ,23))
-	      "23")))
+              "23")))
 
 (test binary-operation-test
   (is (equalp (fmt nil (:b 5))
-	      "101"))
+              "101"))
   (is (equalp (fmt* nil `(:b 5))
-	      "101")))
+              "101")))
 
 (test octal-operation-test
   (is (equalp (fmt nil (:o 20))
-	      "24"))
+              "24"))
   (is (equalp (fmt* nil `(:o 20))
-	      "24")))
+              "24")))
 
 (test hexadecimal-operation-test
   (is (equalp (fmt nil (:x 20))
-	      "14"))
+              "14"))
   (is (equalp (fmt* nil `(:x 20))
-	      "14")))
+              "14")))
 
 (test float-operation-test
   (is (equalp (fmt nil (:f 23.1234))
-	      "23.1234"))
+              "23.1234"))
   (is (equalp (fmt nil (:f 23.1234 :width 5))
-	      "23.12"))
+              "23.12"))
   (is (equalp (fmt nil (:f 23.1234 :width 4 :digits 1))
-	      "23.1")))
+              "23.1")))
 
 (test if-operation-test
   (is (equalp (fmt nil (:if t
-			    (:a "yes")))
-	      "yes"))
+                            (:a "yes")))
+              "yes"))
   (is (equalp (fmt nil (:if t
-			    (:a "yes")
-			    :else
-			    (:a "no")))
-	      "yes"))
+                            (:a "yes")
+                            :else
+                            (:a "no")))
+              "yes"))
   (is (equalp (fmt nil (:if nil
-			    (:a "yes")
-			    :else
-			    (:a "no")))
-	      "no"))
+                            (:a "yes")
+                            :else
+                            (:a "no")))
+              "no"))
   (let ((list (list 1 2 3)))
     (is (equalp (fmt nil (:if (not list)
-			      "none"
-			      :else
-			      (:join "," list)))
-		"1,2,3")))
+                              "none"
+                              :else
+                              (:join "," list)))
+                "1,2,3")))
   (let ((list (list)))
     (is (equalp (fmt nil (:if (not list)
-			      "none"
-			      :else
-			      (:join "," list)))
-		"none"))))
+                              "none"
+                              :else
+                              (:join "," list)))
+                "none"))))
 
 (test times-test
   (is (equalp (fmt nil (:times #\space 5))
-	      "     "))
+              "     "))
   (is (equalp (fmt* nil `(:times #\a 6))
-	      "aaaaaa")))
+              "aaaaaa")))
 
 (test cl-format-test
   (let ((list (list "foo" "bar" "baz")))
     (is (equalp (fmt nil (:format "~{~A~^, ~}" list))
-		(fmt nil (:join ", " list)))))
+                (fmt nil (:join ", " list)))))
   (let ((list (list :foo :bar :baz)))
     (is (equalp (fmt nil (:format "~{~S~^, ~}" list))
-		(fmt nil (:join ", " list (:s _)))))))
+                (fmt nil (:join ", " list (:s _)))))))
